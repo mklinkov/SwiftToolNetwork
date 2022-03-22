ab357d58be031f3cf7dfa6927146249212aceb5d
Maxim Linkov <mlinkov@yotateam.com>
2022-03-22 11:04:08 +0300

    added test for a working example

--- Tests/SwiftToolNetworkTests/SwiftToolNetworkTests.swift
+++ Tests/SwiftToolNetworkTests/SwiftToolNetworkTests.swift
@@ -2,9 +2,25 @@ import XCTest
 @testable import SwiftToolNetwork
 
 final class SwiftToolNetworkTests: XCTestCase {
+    var newData: Data? = nil
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
+        let network: NetworkServiceProtocol = NetworkService()
+        let expectation = expectation(description: "testExample")
+
+        /// this is a stupid test for a working example
+        network.request(method: .get, host: "https://www.google.com/") { [weak self] result in
+            switch result {
+            case .failure(let error):
+                print("error: \(error.localizedDescription)")
+            case .success(let data):
+                print("success:\n\(data)")
+                self?.newData = data
+            }
+            expectation.fulfill()
+
+        }
+        wait(for: [expectation], timeout: 20.0)
+
+        XCTAssertTrue(newData != nil, "Data is empty")
     }
 }
